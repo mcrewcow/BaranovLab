6fd88d958dd688420677307db1b50033b7bdcd70
Emil Kryukov <77118598+mcrewcow@users.noreply.github.com>
2022-10-15 17:34:57 -0400

    description + packages

--- BDNFGDNF.R
+++ BDNFGDNF.R
@@ -1,3 +1,4 @@
+#load the packages for the analysis
 library(Seurat)
 library(patchwork)
 library(dplyr)
@@ -14,11 +15,13 @@ library(SeuratWrappers)
 library(Matrix)
 
 library(dittoSeq)
+library(ggalt) #this one we need for geom_xspline() - smooth version of geom_line()
 
 #load h5seurat files 
 FD59  <- LoadH5Seurat('C://Users/Emil/10X/scretina/FD59.h5Seurat')
-FD59$stage <- 'FD59'
+FD59$stage <- 'FD59' #add stage label
 
+#generate the dual Y axis plot for GDNF-BDNF expression
 gg1 <- ggplot(factorsall, aes(x = Stage, y = GDNF, fill = Gene, color = Gene, group = Gene)) +
  geom_point() + geom_xspline() + scale_y_continuous("BDNF", sec.axis = sec_axis(~./75, name = "GDNF")) + theme_bw()
  
@@ -29,9 +32,10 @@ gg2 <- gg2 <- ggplot(factorsall, aes(x = Stage, y = GDNF, fill = Gene, color = G
 +     axis.title.y=element_blank(),legend.position="none",
 +     panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),
 +     panel.grid.minor=element_blank(),plot.background=element_blank())
+gg1 + gg2
 
-
-FD59$labels <- FD59@active.ident
+#cellchat analysis
+FD59$labels <- FD59@active.ident #save current cluster annotation
 cellchat <- createCellChat(object = FD59, group.by = "labels")
 CellChatDB <- CellChatDB.human
 CellChatDB.use <- CellChatDB
@@ -49,8 +53,8 @@ cellchat <- aggregateNet(cellchat)
 cellchat <- netAnalysis_computeCentrality(cellchat, slot.name = "netP") # the slot 'netP' means the inferred intercellular communication network of signaling pathways
 
 
-pairLR.NT <- extractEnrichedLR(cellchat, signaling = 'NT', geneLR.return = FALSE)
-netAnalysis_contribution(cellchat, signaling = pathways.show)
-LR.show <- pairLR.NT[1,]
+pairLR.NT <- extractEnrichedLR(cellchat, signaling = 'NT', geneLR.return = FALSE) 
+netAnalysis_contribution(cellchat, signaling = pathways.show) #check the pathway for L-R pairs and their contribution
+LR.show <- pairLR.NT[1,] #choose the L-R part needed
 LR.show
-netVisual_individual(cellchat, signaling = 'NT', pairLR.use = LR.show, layout = "chord", show.legend = TRUE)
+netVisual_individual(cellchat, signaling = 'NT', pairLR.use = LR.show, layout = "chord", show.legend = TRUE) #generate the chord plot
