5d65e90809045e8ae834c171ec3e226026a30f80
Emil Kryukov <77118598+mcrewcow@users.noreply.github.com>
2022-10-21 03:05:48 -0400

    from manual to automated approach for L/R RGC/retina expr

--- all_l-r_pairs_cellchat.R
+++ all_l-r_pairs_cellchat.R
@@ -70,3 +70,110 @@ tabfun <- function(matgenerated) {
                    theme(axis.text.y=element_blank(),
                          axis.text.x=element_blank()) + plot_layout(guides = "collect") & theme(legend.position = "bottom")) } 
     print(ggsum)}
+
+
+
+
+CellChatDB <- CellChatDB.human
+interaction_input <- CellChatDB$interaction
+library(dplyr)
+library(tidyr)
+int1 <- interaction_input %>% separate(interaction_name, c('Ligand','R1','R2'))
+
+int2 <- int1[c(1:3)]
+int2 <- head(int2, - 4) 
+lrpairs <- function(tablet) {
+    for(i in 1:length(tablet$Ligand)) {
+        if(is.na(tablet$R2[i]) == FALSE) {
+            tablet <- rbind(tablet, data.frame(Ligand = tablet$Ligand[i], R1 = tablet$R2[i], R2 = tablet$R2[i])) } }
+    return(tablet) }
+
+int2 <- lrpairs(int2)
+df_added <- data.frame(Ligand = c('ITGA4','ITGA9','ITGB1','ITGB7','VSIR'), R1 = c('VCAM1','VCAM1','VCAM1','VCAM1','IGSF11'))
+int3 <- int2[c(1:2)]
+int3 <- rbind(int3, df_added)	 
+human <- merge(FD59, y = c(FD82, FD125, adult))
+humanRGC <- subset(human, idents = c('RGC'))
+human$labels <- human@active.ident
+DefaultAssay(humanRGC) <- 'RNA'
+p <- AverageExpression(humanRGC, features = int3$Ligand, assays = 'RNA', group.by = 'stage')
+p <- as.data.frame(p)
+int3 <- int3 %>% distinct(Ligand, R1, .keep_all = TRUE)
+
+exprassign <- function(tabletexpr, tabletall) {
+    
+    for(i in 1:length(tabletexpr$RNA.Adult)) {
+        for(j in 1:length(tabletall$Ligand)) {
+            if(tabletall$Ligand[j] == rownames(tabletexpr)[i]) {
+                tabletall[j,4] <- tabletexpr[i,1]
+                tabletall[j,5] <- tabletexpr[i,2]
+                tabletall[j,6] <- tabletexpr[i,3]
+                tabletall[j,7] <- tabletexpr[i,4]
+            } } }
+    return(tabletall) } 
+int4$LigandRGCAdult <- 'NA'
+int4$LigandRGCFD125 <- 'NA'
+int4$LigandRGCFD59 <- 'NA'
+int4$LigandRGCFD82 <- 'NA'
+int4 <- exprassign(p, int3)
+	
+p <- AverageExpression(humanRGC, features = int3$R1, assays = 'RNA', group.by = 'stage')
+p <- as.data.frame(p)
+int4$RecRGCAdult <- 'NA'
+int4$RecRGCFD125 <- 'NA'
+int4$RecRGCFD59 <- 'NA'
+int4$RecRGCFD82 <- 'NA'
+exprassign1 <- function(tabletexpr, tabletall, tabletfinal) {
+    
+    for(i in 1:length(tabletexpr$RNA.Adult)) {
+        for(j in 1:length(tabletall$R1)) {
+            if(tabletall$R1[j] == rownames(tabletexpr)[i]) {
+                tabletfinal[j,8] <- tabletexpr[i,1]
+                tabletfinal[j,9] <- tabletexpr[i,2]
+                tabletfinal[j,10] <- tabletexpr[i,3]
+                tabletfinal[j,11] <- tabletexpr[i,4]
+            } } }
+    return(tabletfinal) } 
+int4 <- exprassign1(p, int3, int4)
+
+p <- AverageExpression(human, features = int3$Ligand, assays = 'RNA', group.by = 'stage')
+p <- as.data.frame(p)
+
+int4$LigandAdult <- 'NA'
+int4$LigandFD125 <- 'NA'
+int4$LigandFD59 <- 'NA'
+int4$LigandFD82 <- 'NA'
+
+exprassign2 <- function(tabletexpr, tabletall, tabletfinal) {
+    
+    for(i in 1:length(tabletexpr$RNA.Adult)) {
+        for(j in 1:length(tabletall$Ligand)) {
+            if(tabletall$Ligand[j] == rownames(tabletexpr)[i]) {
+                tabletfinal[j,12] <- tabletexpr[i,1]
+                tabletfinal[j,13] <- tabletexpr[i,2]
+                tabletfinal[j,14] <- tabletexpr[i,3]
+                tabletfinal[j,15] <- tabletexpr[i,4]
+            } } }
+    return(tabletfinal) } 
+int4 <- exprassign2(p, int3, int4)
+
+p <- AverageExpression(human, features = int3$R1, assays = 'RNA', group.by = 'stage')
+p <- as.data.frame(p)
+
+int4$RecAdult <- 'NA'
+int4$RecFD125 <- 'NA'
+int4$RecFD59 <- 'NA'
+int4$RecFD82 <- 'NA'
+
+exprassign3 <- function(tabletexpr, tabletall, tabletfinal) {
+    
+    for(i in 1:length(tabletexpr$RNA.Adult)) {
+        for(j in 1:length(tabletall$R1)) {
+            if(tabletall$R1[j] == rownames(tabletexpr)[i]) {
+                tabletfinal[j,16] <- tabletexpr[i,1]
+                tabletfinal[j,17] <- tabletexpr[i,2]
+                tabletfinal[j,18] <- tabletexpr[i,3]
+                tabletfinal[j,19] <- tabletexpr[i,4]
+            } } }
+    return(tabletfinal) } 
+int4 <- exprassign3(p, int3, int4)
