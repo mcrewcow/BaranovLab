10d5324cf14c394d8cf48f4bda5ae7af43be219a
Emil Kryukov <77118598+mcrewcow@users.noreply.github.com>
2022-11-28 09:27:10 -0500

    Update escape_peaks_subset.R

--- escape_peaks_subset.R
+++ escape_peaks_subset.R
@@ -170,6 +170,56 @@ gene_list <- c('ADORA1',	'ADRA2A',	'ADRB2',	'AGTR2',	'AHSG',	'AKT1',	'AKT2',	'AL
                'Snhg11','Gadd45a','Mdk','Prdx1','Gm10260','Hes6','H2afv','Dlx2','Hes5','Btg2','Mfng','Btbd17',
                'Rassf4','Ccnd1','Itm2a','Hmgn3','Tead2','Alyref','Sfrp2','Fos')
 
+#for human
+
+gene_list <- c('ADORA1',	'ADRA2A',	'ADRB2',	'AGTR2',	'AHSG',	'AKT1',	'AKT2',	'ALK',	'FASLG',	'AR',	'AREG',	'ARNT',	'AXL',	'BDKRB2',
+               'CEACAM1',	'BLK',	'BRAF',	'DDR1',	'RUNX2',	'CD4',	'CD7',	'CD8A',	'CD8B',	'CD63',	'CDH3',	'CDH13',	'CHN1',	'CHRNA3',
+               'AP3S1',	'CSF1R',	'CSPG4',	'CCN2',	'CTNNB1',	'DOK1',	'EFNA1',	'EFNA2',	'EFNA3',	'EFNA4',	'EFNA5',	'EFNB1',	'EFNB2',
+               'EFNB3',	'EGFR',	'EPHA2',	'EPHA1',	'EPHA3',	'EPHA4',	'EPHA5',	'EPHA7',	'EPHA8',	'EPHB1',	'EPHB2',	'EPHB3',	'EPHB4',
+               'EPHB6',	'ERBB2',	'ERBB3',	'ERBB4',	'EFEMP1', 'FER',	'FES',	'FGFR1',	'FGFR3',	'FGFR2',	'FGFR4',	'FGR',	'FLT1',	'FLT3',
+               'FLT4',	'FRK',	'FUT7',	'GATA3',	'GFRA1',	'GFRA2',	'GFRA3',	'GHR',	'GHRHR',	'GHSR',	'GPR21',	'GPER1',	'FFAR3',	'GRB2',
+               'GRB7',	'GRB10',	'GRB14',	'HCK',	'NRG1',	'NDST1',	'IGF1R',	'IGF2R',	'IGFBP1',	'IGFBP2',	'IGFBP3',	'IGFBP4',	'IGFBP5',
+               'IGFBP6',	'RBPJ',	'INSR',	'INSRR',	'IRS1',	'ITGA1',	'ITGA5',	'ITGB3',	'JAK2',	'JAK3',	'KDR',	'KIT',	'STMN1', 'LCK',	'LEP',
+               'LRP1',	'LTK',	'LYN',	'MET',	'FOXO4',	'MST1R',	'MUSK',	'NDN',	'NEDD9',	'NGFR',	'NKX3-1',	'NTRK1',	'NTRK2',	'NTRK3',	'ROR1',
+               'ROR2',	'DDR2',	'PAK1',	'PAK2',	'PAK3',	'PDGFRA',	'PDGFRL',	'PDGFRB',	'PIGR',	'PIK3C2A',	'PIK3CA',	'PIK3CB',	'PIK3CD',	'PIK3R1',
+               'PIK3R2',	'PLAUR',	'PRLR',	'PSEN1',	'PTGIR',	'PTPN1',	'PTPN2',	'PTPN3',	'PTPN11',	'PTPN12',	'PTPRA',	'PTPRE',	'PTPRG',	'PTPRJ',
+               'PTPRR',	'RAC1',	'RAF1',	'RARRES2',	'RET',	'ROBO1',	'ROS1',	'RYK', 'SORT1',	'SORL1',	'SOS1',	'SOX9',	'SRC',	'SREBF1',	'SRMS',
+               'STAT3',	'STAT5A',	'STAT5B',	'STAT6',	'TEK',	'TIAM1',	'TIE1',	'TYRO3',	'VTN',	'WNT1',	'WNT5A',	'YES1',	'RAB7A',	'FZD4',	'PIK3R3',
+               'SOCS1',	'IRS2',	'TNK1',	'NRP2',	'NRP1',	'SOCS2',	'HAP1',	'MPZL1',	'SOCS3',	'HIP1R',	'GPRC5A',	'FIBP',	'REPS2',	'NOG',	'KL', 'MVP',	
+               'NR1H4',	'FGFBP1',	'NAMPT',	'TNK2',	'SPRY3',	'SPRY1',	'SPRY2',	'CNKSR1',	'STUB1',	'EFS',	'MERTK',	'SH2B2',	'TXNIP',	'RGS14',	'NRG3',
+               'FRS3',	'FRS2',	'NEU3',	'EMILIN1',	'PTPRT',	'PTP4A3',	'LMTK2',	'SIRT2',	'SETX',	'SIK2',	'ANKS1A',	'SIRT1',	'LEPROTL1',	'FLRT3',	'FLRT2',
+               'FLRT1',	'SHC2',	'DSTYK',	'NGEF',	'PRKD2',	'ADGRA2',	'GREM1',	'CYFIP2',	'DNAI1',	'NPTN',	'SNX5',	'DLL1',	'PILRB',	'ADIPOR1',	'CRIM1',
+               'GHRL',	'FGFRL1',	'ERRFI1',	'WNT4',	'LEPROT',	'STYK1',	'SMPD3',	'ERBIN',	'SULF2',	'RTN4',	'SEMA6A',	'JCAD',	'NCOA5',	'SNX6',	'SMOC2',
+               'GFRA4',	'HHIP',	'GIGYF1',	'GKAP1',	'NDEL1',	'MVB12B',	'OSBPL8',	'RASGRP4',	'NUS1',	'CLNK',	'ZFYVE27',	'SOCS4',	'OTOL1',	'IL31RA',	
+               'SAMD10',	'SOGA1',	'FGFBP3',	'SESN3',	'DAB2IP',	'CLEC14A',	'CADM4',	'MUC20',	'DOK6',	'FAM83B',	'STXBP4',	'EPHA10',	'EPHA6',	'LRIT3',
+               'GFRAL', 'ADRB2','ANGPT1','ANGPT4','CHRNA3','DGKQ','EFNA5','EGF','GREM1','NRG1','NRG3','PDGFC','PILRB','PRLR','TAL1', 'AGT','AGTR2',
+               'AKT1S1','BCAR1','BDNF','CASP3','CORO1A','CYFIP1','CYFIP2','DDIT4','DOK5','GFRA1','HAP1','KIDINS220','MAGI2','NDN','NGF','NTF3','NTF4',
+               'NTRK1','NTRK2','NTRK3','PPP2R5B','PTPN11','RAF1','RAP1A','RAPGEF1','RAPGEF2','SORT1','SOS1','SPRY1','SPRY2','SRC','TMEM108','WASF1',
+               'ZDHHC17','ZFYVE27', 'ADAM17','AGR2','APP','AREG','ARF4','ARTN','ATXN2','BTC','CADM4','CBLC','CCDC88A','CD2AP','CD300LF','CDH5','CNOT9',
+               'CNTF','CSF2','CSF3','DAB2IP','ECM1','EFEMP1','EGF','EPGN','ERAP1','ERBB4','EREG','ERN1','ESM1','FAM83B','FER','FGF1','FGF10','FGF16',
+               'FGF17','FGF18','FGF19','FGF2','FGF20','FGF21','FGF22','FGF23','FGF3','FGF4','FGF5','FGF6','FGF7','FGF8','FGF9','FLRT2','FLRT3','FRS2',
+               'FRS3','FYN','GATA3','GDNF','GLMN','GRB2','GREM1','HBEGF','HIP1','IL10','IL11','IL12A','IL12B','IL12RB1','IL1A','IL1B','IL1F10','IL1R1',
+               'IL1RAP','IL1RN','IL2','IL21','IL23R','IL27RA','IL3','IL36A','IL36B','IL36G','IL36RN','IL37','IL4','IL5','IL6','IL6R','IL6ST','IL7','IL9',
+               'IRAK4','ITGA5','ITGB3','JAK2','KL','KLB','LINGO1','LYN','MS4A1','MYD88','NCSTN','NPTN','NRTN','PDCL3','PDGFA','PDGFB','PDGFC','PDGFD',
+               'PDGFRA','PDGFRB','PGF','PIBF1','PLSCR1','PSEN1','PSPN','PTPRJ','PYCARD','RNF126','RNF41','SDCBP','SHC1','SLC9A3R1','SNX1','SNX2','SNX4',
+               'SOCS5','SRC','TGFA','TIMM50','TLR5','TLR9','TNK2','TOLLIP','TRIP6','TSLP','VAV2','VAV3','VEGFA','VEGFB','VEGFC','VEGFD', 'AGT','AGTR2',
+               'AKT1S1','BCAR1','BDNF','CASP3','CORO1A','CYFIP1','CYFIP2','DDIT4','DOK5','GFRA1','HAP1','KIDINS220','MAGI2','NDN','NGF','NTF3','NTF4',
+               'NTRK1','NTRK2','NTRK3','PPP2R5B','PTPN11','RAF1','RAP1A','RAPGEF1','RAPGEF2','SORT1','SOS1','SPRY1','SPRY2','SRC','TMEM108','WASF1',
+               'ZDHHC17','ZFYVE27', 'ARF6', 'RAB3A', 'CTBP2', 'ERC1', 'PCLO', 'DBN1', 'DBNL', 'INA', 'RAB3A','CBLN2','CTBP2','DBN1','ARF6','TAGLN3',
+               'ERC1','CTBP2','PCLO','MALAT1','MT-CYB', 'DNAJC6','SAMD4A','STAT3','RALBP1',
+             #from here after cellchat 
+               'Ncam1', 'L1cam', 'Nrxn1', 'Nlgn1', 'Mpzl1', 'Efna5', 'Nrxn3', 'Nrxn2', 'Ephb2', 'Cntn2', 'Cntnap2', 'Mstn', 'Tgfbr1', 'Acvr2b',
+               #from here after individual pathway DEGs 
+               'RAB3A','CBLN2','RPS4Y1',
+               'CPLX2','RPS4Y1','DBN1','CLEC2D','EIF5A','RPL17','AC009501.4','RPS10','TUBB3','NBEAL1','MIF','FKBP1A','ERC1','CTBP2','PCLO','RPS4Y1','ATP6V1G2','FKBP1B',
+               'ARF6', 'RPS4Y1','FARP1','MEG3','NEFL','APLP2','APP','INA','SNAP25','LY6H','ATP1B1','ARL6IP5','PRNP','ITM2C','SERPINE2','GABBR2','PCP4',
+               'REEP5','SPTBN1','PIK3R1','NEUROD1','FABP7','ERC1','CTBP2','PCLO','RPS4Y1','ATP6V1G2','FKBP1B')
+
+
+
+
+
+)
 
 gene_list <- as.list(gene_list)
 gene_list  <- lapply(gene_list , tolower) 
