6ad38fffaff2d1f03a95bb68ed07bc60c1227031
Emil Kryukov <77118598+mcrewcow@users.noreply.github.com>
2023-01-20 14:06:11 -0500

    Create brain_microglia.R

---
+++ brain_microglia.R
@@ -0,0 +1,2 @@
+#download the datasets from Hammond et al, 2019 https://github.com/samuel-marsh/Hammond-et-al_2019_Microglia_scRNAseq
+wget -O Hammond_et-al-2019_Seurat_Converted_v4.qs https://figshare.com/ndownloader/files/37624052
